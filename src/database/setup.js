1cdd92dbfd02649fb50a634a9eece96edc6f09bb
TheycallmeNate <nathanmangal@gmail.com>
2021-09-02 17:39:06 -0400

    deleted test.txt added setup for db

---
+++ src/database/setup.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const connectionString = process.env.DATABASE_URL;
+module.exports = function () {
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+    .then(() => {
+      console.log('Mongo Connection Open!');
+    })
+    .catch((err) => {
+      console.log('Mongo Connection Error!', err);
+    });
+};
