c07e2bab84a5be3c9b598b1a82e74229a9417e13
TheycallmeNate <nathanmangal@gmail.com>
2021-09-13 19:30:38 -0400

    added route for age eligibility, moved age check from authController to authorization middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 // const { check } = require('express-validator');
 const router = express.Router();
 const Auth = require("../controllers/authControllers.js");
+const Authorization = require("../middleware/authorization");
+
+router.post("/eligibility/age", Authorization.age);
 
 router.post("/signup", Auth.signup);
 
