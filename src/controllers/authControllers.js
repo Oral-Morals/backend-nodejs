b12327f295b5b105c2e28bb155f3102743f4ecaf
TheycallmeNate <nathanmangal@gmail.com>
2021-09-14 17:37:55 -0400

    moved code from src/middleware/authorization.js to src/controllers/authControllers.js

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -57,3 +57,58 @@ exports.login = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+exports.authorizeAge = async (req, res, next) => {
+  try {
+    // Check if the user's age is under 18 years old
+
+    const userDOB = moment(req.body.dateOfBirth);
+    const earliestDOB = moment().subtract(18, "years");
+
+    // If the user's DOB is after the earliest DOB to be 18 years old today,
+    // the user is younger than 18 years old.
+    if (userDOB.isAfter(earliestDOB)) {
+      return res.status(403).json({ success: false, message: "User must be 18 or older to use app." });
+    }
+
+    // If the user's DOB is valid, the response is 200 and DOB is sent back to be used in the registration flow.
+    if (!userDOB.isAfter(earliestDOB)) return res.status(200).json({ success: true, data: { dateOfBirth: userDOB } });
+
+    // Go to next available middleware.
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+exports.authorizeUser = async (req, res, next) => {
+  try {
+    // Check if authorization header credentials were sent with request.
+    if (!req.headers.authorization) return res.status(401).json({ message: "Authorization header required" });
+
+    // Check if the authorization type is Bearer.
+    if (!req.headers.authorization.startsWith("Bearer "))
+      return res.status(401).json({ message: "Authorization format is: Bearer <token>" });
+
+    // Get the token from the authorization header by removing "Bearer " from the string.
+    let token = req.headers.authorization.replace("Bearer ", "");
+
+    // Returns the payload if the signature is valid.
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) return res.status(500).json({ error });
+
+      // Check for invalid token.
+      if (!decodedToken) return res.status(401).json({ message: "Invalid authorization token. Please login." });
+
+      // The decodedToken values are the user's details.
+      req.user = decodedToken;
+
+      // Go to next available middleware.
+      next();
+    });
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: "Server error. Possible error with token." });
+  }
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 // const { check } = require('express-validator');
 const router = express.Router();
 const Auth = require("../controllers/authControllers.js");
-const Authorization = require("../middleware/authorization");
 
-router.post("/eligibility/age", Authorization.age);
+router.post("/eligibility/age", Auth.authorizeAge);
 
 router.post("/signup", Auth.signup);
 
